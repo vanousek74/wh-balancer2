0addce30289d726824787f8025866bc12592aba4
vanousek74 <d.a.n.n.y74@seznam.cz>
2025-07-26 14:32:24 +0200

    Add files via upload

---
+++ wh-balancer.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         WH Balancer (vanousek74)
+// @namespace    https://vanousek74.github.io/
+// @version      1.0
+// @description  Balancování surovin mezi vesnicemi s pokročilými možnostmi (výstavba, rekrut, zůstatek atd.)
+// @author       vanousek74 & ChatGPT
+// @match        https://*.divokekmeny.cz/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Nastavení skriptu
+    const config = {
+        excludeConstruction: true,
+        excludeRecruitment: true,
+        keepGoldCoin: true,
+        keepPercentage: 10,
+        useGroupAsSource: false,
+        sourceGroup: '',
+        useGroupAsTarget: false,
+        targetGroup: '',
+        prioritiseDistance: true
+    };
+
+    alert("Skript byl úspěšně načten! Funkce budou přidány v další verzi.");
+
+    // Budoucí logika balancování zde...
+
+})();
